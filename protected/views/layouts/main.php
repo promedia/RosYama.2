97685abd0c4c3ccbb58b25db5bfd3333420f6270
Aleksandr Shcherbakov <a.shcherbakov@properm.ru>
2012-02-26 17:03:18 +0600

    Removed reformal and GA counter

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -107,7 +107,8 @@
 			</div>
 		</div>
 	</div>	
-		<?php echo $content; ?>
+  
+  <?php echo $content; ?>
 
 	<div class="footer">
 		<div class="container">
@@ -124,42 +125,7 @@
 			<p class="friends">Чиним ямы <a href="http://ukryama.com/">в Украине</a>, <a href="http://belyama.by/">Беларуси</a> и <a href="http://kazyama.kz/">Казахстане</a></p>
 		</div>
 	</div>
-	<script type="text/javascript">
-	reformal_wdg_domain    = "rosyama";
-	reformal_wdg_mode    = 0;
-	reformal_wdg_title   = "rosyama";
-	reformal_wdg_ltitle  = "Оставьте отзыв";
-	reformal_wdg_lfont   = "";
-	reformal_wdg_lsize   = "";
-	reformal_wdg_color   = "#FFA000";
-	reformal_wdg_bcolor  = "#516683";
-	reformal_wdg_tcolor  = "#FFFFFF";
-	reformal_wdg_align   = "left";
-	reformal_wdg_charset = "utf-8";
-	reformal_wdg_waction = 0;
-	reformal_wdg_vcolor  = "#9FCE54";
-	reformal_wdg_cmline  = "#E0E0E0";
-	reformal_wdg_glcolor  = "#105895";
-	reformal_wdg_tbcolor  = "#FFFFFF";
-	
-	reformal_wdg_bimage = "fb17bdca7e3a07420c91c07d5ef7e4f4.png";
-	
-	</script>
-	
-	<script type="text/javascript" language="JavaScript" src="http://reformal.ru/tab6.js?charset=utf-8"></script><noscript><a href="http://rosyama.reformal.ru">rosyama feedback </a> <a href="http://reformal.ru"><img src="http://reformal.ru/i/logo.gif" /></a></noscript>
-	<script type="text/javascript">
-	
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-21943923-3']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
-	</script>
+
 	<? if (!Yii::app()->user->isGuest && $flash=Yii::app()->user->getFlash('user')):?>
 		<div id="addDiv">
 			<div id="fon">
